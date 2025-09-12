e9900108a9a697643dbfa2a5b9791dabc1039251
VHrishita <vempalihrishita05@gmail.com>
2025-09-12 21:49:20 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+    toggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+    });
+});
